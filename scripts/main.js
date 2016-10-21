3071068561954cf55278aef8ccbfe6cd00409f66
Barrowclift <marc@barrowclift.me>
2016-10-20 22:23:27 -0400

    Major form update, text changes, further tweaks to modal

--- scripts/main.js
+++ scripts/main.js
@@ -8,12 +8,28 @@ var KEY_CODE_LEFT_ARROW = 37
 var KEY_CODE_RIGHT_ARROW = 39
 var KEY_CODE_ESCAPE = 27
 var MIN_NUMBER_OF_GUESTS = 1
-var MAX_NUMBER_OF_GUESTS = 20
+var MAX_NUMBER_OF_GUESTS = 5
 
 var INPUT_NAME_ID = "entry.189008425"
 var INPUT_EMAIL_ID = "entry.1176921021"
-var INPUT_MEAL_ID = "entry.368489061"
+var INPUT_ATTENDEE_COUNT_ID = "entry.1367995216"
 var RADIO_BUTTON_ATTEND_ID = "entry.644721739"
+var INPUT_PERSON_1_NAME_ID = "entry.1331495851"
+var SELECT_PERSON_1_FOOD_ID = "entry.812334325"
+var INPUT_PERSON_2_NAME_ID = "entry.1879805707"
+var SELECT_PERSON_2_FOOD_ID = "entry.313573241"
+var INPUT_PERSON_3_NAME_ID = "entry.407509418"
+var SELECT_PERSON_3_FOOD_ID = "entry.530507509"
+var INPUT_PERSON_4_NAME_ID = "entry.162839601"
+var SELECT_PERSON_4_FOOD_ID = "entry.1440417000"
+var INPUT_PERSON_5_NAME_ID = "entry.464247127"
+var SELECT_PERSON_5_FOOD_ID = "entry.192316455"
+var PERSON_ARRAY = ["",
+					INPUT_PERSON_1_NAME_ID,
+					INPUT_PERSON_2_NAME_ID,
+					INPUT_PERSON_3_NAME_ID,
+					INPUT_PERSON_4_NAME_ID,
+					INPUT_PERSON_5_NAME_ID]
 var BUTTON_SUBMIT_ID = "submit"
 
 var FORM_GROUP_WILL_ATTEND_ID = "will-attend"
@@ -48,15 +64,37 @@ function sanitizeNumberOfGuests(event) {
 	if (event.target.value == "") {
 		// If nothing was provided, default to the minimum number of guests
 		event.target.value = MIN_NUMBER_OF_GUESTS.toString()
+		showFoodOptions(event)
 	} else {
 		var userValue = parseInt(event.target.value);
 
 		// Ensuring the user value is within acceptable ranges, fixing it if it's not
 		if (userValue < MIN_NUMBER_OF_GUESTS) {
 			event.target.value = MIN_NUMBER_OF_GUESTS.toString()
+			showFoodOptions(event)
 		} else if (userValue > MAX_NUMBER_OF_GUESTS) {
 			event.target.value = MAX_NUMBER_OF_GUESTS.toString()
+			showFoodOptions(event)
 		}
+	}
+}
+
+function showFoodOptions(event) {
+	if (event.target.value != ""
+		&& event.target.value >= MIN_NUMBER_OF_GUESTS
+		&& event.target.value <= MAX_NUMBER_OF_GUESTS) {
+		// Resetting all food choice inputs to be hidden, only want to show the number that
+		// the user denoted would be showing up
+		for (var i = 1; i <= parseInt(MAX_NUMBER_OF_GUESTS); i++) {
+			document.getElementById("person-"+i.toString()).style.display = "none";
+		}
+
+		// Showing only the number of food choice inputs the user denoted would be showing up
+		for (var i = 1; i <= parseInt(event.target.value); i++) {
+			document.getElementById("person-"+i.toString()).style.display = "block";
+		}
+
+		enableSubmissionButtonIfReady()
 	}
 }
 
@@ -70,11 +108,29 @@ function enableSubmissionButtonIfReady(event) {
 	if (document.getElementById(INPUT_NAME_ID).value != ""
 	 && document.getElementById(INPUT_EMAIL_ID).value != "") {
 		if (document.getElementById(RADIO_BUTTON_ATTEND_ID).checked) {
-			if (document.getElementById(INPUT_MEAL_ID).value == "") {
-				document.getElementById(BUTTON_SUBMIT_ID).disabled = true;
-			} else {
-				document.getElementById(BUTTON_SUBMIT_ID).disabled = false;
+			var attendeeCount = document.getElementById(INPUT_ATTENDEE_COUNT_ID).value
+			for (var i = 1; i <= attendeeCount; i++) {
+				if (document.getElementById(PERSON_ARRAY[i]).value == "") {
+					document.getElementById(BUTTON_SUBMIT_ID).disabled = true;
+					return;
+				}
 			}
+			document.getElementById(BUTTON_SUBMIT_ID).disabled = false;
+
+			// if ((document.getElementById(INPUT_PERSON_1_NAME_ID).style.display === "block"
+			// 	       && document.getElementById(INPUT_PERSON_1_NAME_ID).value != "")
+			//  && (document.getElementById(INPUT_PERSON_2_NAME_ID).style.display === "block"
+			// 	       && document.getElementById(INPUT_PERSON_2_NAME_ID).value != "")
+			//  && (document.getElementById(INPUT_PERSON_3_NAME_ID).style.display === "block"
+			// 	       && document.getElementById(INPUT_PERSON_3_NAME_ID).value != "")
+			//  && (document.getElementById(INPUT_PERSON_4_NAME_ID).style.display === "block"
+			// 	       && document.getElementById(INPUT_PERSON_4_NAME_ID).value != "")
+			//  && (document.getElementById(INPUT_PERSON_5_NAME_ID).style.display === "block"
+			// 	       && document.getElementById(INPUT_PERSON_5_NAME_ID).value != "")) {
+			// 	document.getElementById(BUTTON_SUBMIT_ID).disabled = false;
+			// } else {
+			// 	document.getElementById(BUTTON_SUBMIT_ID).disabled = true;
+			// }
 		} else {
 			document.getElementById(BUTTON_SUBMIT_ID).disabled = false;
 		}
